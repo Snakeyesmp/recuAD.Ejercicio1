cb5a5b87a72181902143a52b551fc5ddb14894f3
Mario Muñoz <snakeyes271101@gmail.com>
2024-02-16 17:54:54 +0100

    Reducidos comentarios

--- src/main/java/principal/Principal.java
+++ src/main/java/principal/Principal.java
@@ -39,34 +39,11 @@ public class Principal {
 	private static Session sesion;
 
 	// MONGODB
-
 	private static MongoDatabase database;
 	private static MongoClient mongoClient;
 
 	public static void main(String[] args) {
-
-		/*
-		 * PARA LEER LOS DATOS DE MYSQL System.out.println("----- Capitales ----- ");
-		 * List<Capitales> listaCapitales = hibernateObtenerCapitales(); for (Capitales
-		 * capital : listaCapitales) { System.out.println(capital.getNombre()); }
-		 * 
-		 * System.out.println("----- Poblaciones -----"); List<Poblaciones>
-		 * listaPoblaciones = hibernateObtenerPoblaciones(); for (Poblaciones poblacion
-		 * : listaPoblaciones) { System.out.println(poblacion.getNombre());
-		 * 
-		 * }
-		 */
-
 		mongoConectarOnline();
-
-		menu();
-
-		mongoCerrarConexion(); // Cerrar conexion MongoDB
-		sesion.close(); // Cerrar conexion Hibernate
-
-	}
-
-	public static void menu() {
 
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Introduce el nombre de la capital");
@@ -75,17 +52,12 @@ public class Principal {
 		metodoAglutinador(nombreCapital);
 
 		sc.close();
-
+		mongoCerrarConexion(); // Cerrar conexion MongoDB
+		sesion.close(); // Cerrar conexion Hibernate
 	}
 
 	// ------------------------------------------------------------
-	// ------------------------------------------------------------
-	// ------------------------------------------------------------
-	// ------------------------------------------------------------
-	// ------------------------------------------------------------
 	// -------------------- MÉTODO AGLUTINADOR --------------------
-	// ------------------------------------------------------------
-	// ------------------------------------------------------------
 	// ------------------------------------------------------------
 	public static void metodoAglutinador(String nombreCapital) {
 		Capitales capitalAinsertar = new Capitales();
@@ -96,9 +68,7 @@ public class Principal {
 		if (hibernateExisteCapital(nombreCapital)) {
 			System.out.println("La capital '" + nombreCapital + "' ya está en MySQL");
 
-			// ------- BUSCAR POBLACIONES EN MySQL --------
 			List<Poblaciones> listaPoblaciones = hibernateObtenerPoblaciones();
-
 			for (Poblaciones poblacion : listaPoblaciones) {
 				mongoComprobarOCrearPoblacion(poblacion);
 			}
@@ -110,7 +80,7 @@ public class Principal {
 
 			if (hibernateInsertarCapital(capitalAinsertar)) {
 				System.out.println("La capital '" + nombreCapital + "' se ha insertado en MySQL");
-				// Comprobar si la capital existe en MongoDB y si no, crearla
+
 				mongoComprobarOCrearCapital(capitalAinsertar);
 
 			} else {
@@ -120,19 +90,10 @@ public class Principal {
 		}
 
 	}
-	// ------------------------------------------------------------
-	// ------------------------------------------------------------
-	// ------------------------------------------------------------
-	// ------------------------------------------------------------
-	// ------------------------------------------------------------
-	// -------------------- HIBERNATE ----------------------------
-	// ------------------------------------------------------------
-	// ------------------------------------------------------------
-	// ------------------------------------------------------------
-	// ------------------------------------------------------------
-	// ------------------------------------------------------------
 
-	// Obtener todas las capitales, se puede usar para saber si están repetidas
+	// ------------------------------------------------------------
+	// --------------------- HIBERNATE ----------------------------
+	// ------------------------------------------------------------
 	public static List<Capitales> hibernateObtenerCapitales() {
 		try {
 			sesion = sf.openSession();
@@ -146,7 +107,6 @@ public class Principal {
 		}
 	}
 
-	// Obtener todas las poblaciones, se puede usar para saber si están repetidas
 	public static List<Poblaciones> hibernateObtenerPoblaciones() {
 		try {
 			sesion = sf.openSession();
@@ -160,42 +120,21 @@ public class Principal {
 		}
 	}
 
-	/**
-	 * Le pasas el nombre de la capital, comprueba en la bbdd ese campo
-	 * 
-	 * @param nombreCapital
-	 * @return true si existe, false si no
-	 */
 	public static boolean hibernateExisteCapital(String nombreCapital) {
 
 		List<Capitales> listaCapitales = hibernateObtenerCapitales();
-
 		for (Capitales capitalActual : listaCapitales) {
 
 			if (capitalActual.getNombre().equals(nombreCapital))
 				return true;
-
 		}
 		return false;
-
 	}
 
 	// ------------------------------------------------------------
-	// ------------------------------------------------------------
-	// ------------------------------------------------------------
-	// ------------------------------------------------------------
-	// ------------------------------------------------------------
 	// -------------------- CRUD MySQL ----------------------------
 	// ------------------------------------------------------------
-	// ------------------------------------------------------------
-	// ------------------------------------------------------------
-	// ------------------------------------------------------------
-	// ------------------------------------------------------------
 
-	/**
-	 * 
-	 * Le pasas un objeto de capital y la inserta en hibernate
-	 */
 	public static boolean hibernateInsertarCapital(Capitales capital) {
 		try {
 			sesion = sf.openSession();
@@ -211,7 +150,6 @@ public class Principal {
 		}
 	}
 
-	// Leer una capital por su nombre
 	public static Capitales hibernateLeerCapital(String nombreCapital) {
 		sesion = sf.openSession();
 		Capitales capital = sesion.createQuery("FROM Capitales WHERE nombre = :nombre", Capitales.class)
@@ -220,7 +158,6 @@ public class Principal {
 		return capital;
 	}
 
-	// Actualizar una capital
 	public static boolean hibernateActualizarCapital(String nombreActual, String nombreNuevo) {
 		try {
 			sesion = sf.openSession();
@@ -242,7 +179,6 @@ public class Principal {
 		}
 	}
 
-	// Eliminar una capital
 	public static boolean hibernateEliminarCapital(String nombreCapital) {
 		try {
 			sesion = sf.openSession();
@@ -264,23 +200,9 @@ public class Principal {
 	}
 
 	// ------------------------------------------------------------
-	// ------------------------------------------------------------
-	// ------------------------------------------------------------
-	// ------------------------------------------------------------
-	// ------------------------------------------------------------
-	// --------------------- MONGODB ------------------------------
-	// ------------------------------------------------------------
-	// ------------------------------------------------------------
-	// ------------------------------------------------------------
-	// ------------------------------------------------------------
+	// ----------------------- MONGODB ----------------------------
 	// ------------------------------------------------------------
 
-	/**
-	 * Conectar a la BBDD de
-	 * https://cloud.mongodb.com/v2/65aea9f73f5c6d633cf14665#/clusters/detail/Cluster0
-	 * 
-	 * @return
-	 */
 	public static boolean mongoConectarOnline() {
 		String connectionString = "mongodb+srv://mariomunozpequeno:4rtlus9pq7UjxKNO@cluster0.xu4apmq.mongodb.net/?retryWrites=true&w=majority";
 		ServerApi serverApi = ServerApi.builder().version(ServerApiVersion.V1).build();
@@ -300,11 +222,6 @@ public class Principal {
 		}
 	}
 
-	/**
-	 * Conectarse a la BBDD local de MongoDB
-	 * 
-	 * @return
-	 */
 	public static boolean mongoConectarLocal() {
 		try {
 			mongoClient = MongoClients.create();
@@ -315,7 +232,6 @@ public class Principal {
 			e.printStackTrace();
 			return false;
 		}
-
 	}
 
 	public static void mongoCerrarConexion() {
@@ -358,48 +274,27 @@ public class Principal {
 		return listaDocumentos;
 	}
 
-	/**
-	 * Comprueba si el parámetro está en la tabla Capitales de MongoDB, si no está
-	 * lo crea
-	 * 
-	 * @param capital
-	 * @return
-	 */
+	
 	public static boolean mongoComprobarOCrearCapital(Capitales capital) {
-		// Obtener la colección "capitales" de la base de datos
+
 		MongoCollection<Document> capitalesCollection = database.getCollection("capitales");
 
-		// Buscar en la colección "capitales" un documento que tenga el mismo nombre que
-		// la capital
 		Document capitalExistente = capitalesCollection.find(Filters.eq("nombre", capital.getNombre())).first();
 
-		// Si se encuentra un documento existente
 		if (capitalExistente != null) {
-			// Imprimir un mensaje informando que la capital ya existe en MongoDB
 			System.out.println("La capital '" + capital.getNombre() + "' ya existe en MongoDB.");
 			return true;
 		}
-		// Si no se encuentra un documento existente
 		else {
-			// Crear un nuevo documento con el nombre de la capital
 			Document nuevaCapital = new Document().append("nombre", capital.getNombre());
 
-			// Insertar el nuevo documento en la colección "capitales"
 			capitalesCollection.insertOne(nuevaCapital);
 
-			// Imprimir un mensaje informando que la capital ha sido creada en MongoDB
 			System.out.println("La capital '" + capital.getNombre() + "' ha sido creada en MongoDB.");
 			return false;
 		}
 	}
 
-	/**
-	 * Comprueba si el parámetro está en la tabla Poblaciones de MongoDB, si no está
-	 * lo crea
-	 * 
-	 * @param nombrePoblacion
-	 * @return
-	 */
 	public static boolean mongoComprobarOCrearPoblacion(Poblaciones poblacion) {
 		MongoCollection<Document> poblacionesCollection = database.getCollection("poblaciones");
 		MongoCollection<Document> capitalesCollection = database.getCollection("capitales");
@@ -410,25 +305,19 @@ public class Principal {
 			System.out.println("La población '" + poblacion.getNombre() + "' ya existe en MongoDB.");
 			return true;
 		} else {
-			// Comprobar si la capital existe en MongoDB y si no, crearla
 			mongoComprobarOCrearCapital(poblacion.getCapitales());
 
-			// Buscar la capital en MongoDB y obtener su ObjectID
 			Document capitalMongoDB = capitalesCollection
 					.find(Filters.eq("nombre", poblacion.getCapitales().getNombre())).first();
 			ObjectId capitalId = capitalMongoDB.getObjectId("_id");
 
-			// Buscar la capital en MongoDB utilizando el ObjectId obtenido
 			Document capitalComprobada = capitalesCollection.find(Filters.eq("_id", capitalId)).first();
 
-			// Comprobar que el nombre de la capital obtenida coincide con el nombre de la
-			// capital de la población
 			if (!capitalComprobada.getString("nombre").equals(poblacion.getCapitales().getNombre())) {
 				System.out.println("El ObjectId de la capital '" + poblacion.getCapitales().getNombre()
 						+ "' no corresponde a la capital correcta en MongoDB para la población '"
 						+ poblacion.getNombre() + "'.");
 
-				// Corregir la referencia errónea
 				Bson filter = Filters.eq("nombre", poblacion.getNombre());
 				Bson updateOperation = Updates.set("capital", capitalId);
 				poblacionesCollection.updateOne(filter, updateOperation);
@@ -439,7 +328,7 @@ public class Principal {
 
 			Document nuevaPoblacion = new Document().append("nombre", poblacion.getNombre())
 					.append("poblacion", poblacion.getCodPoblacion())
-					.append("capital", capitalId); // Usar el ObjectID de la capital
+					.append("capital", capitalId); 
 
 			poblacionesCollection.insertOne(nuevaPoblacion);
 			System.out.println("La población '" + poblacion.getNombre() + "' ha sido creada en MongoDB.");
@@ -453,21 +342,17 @@ public class Principal {
 		MongoCollection<Document> poblacionesCollection = database.getCollection("poblaciones");
 		MongoCollection<Document> capitalesCollection = database.getCollection("capitales");
 
-		// Buscar la población en MongoDB
 		Document poblacionMongoDB = poblacionesCollection
 				.find(Filters.eq("nombre", poblacion.getNombre())).first();
 
-		// Si la población no existe, crearla
 		if (poblacionMongoDB == null) {
-			// Buscar la capital en MongoDB y obtener su ID numérico
 			Document capitalMongoDB = capitalesCollection
 					.find(Filters.eq("nombre", poblacion.getCapitales().getNombre())).first();
 			int capitalId = capitalMongoDB.getInteger("id");
 
 			Document nuevaPoblacion = new Document().append("nombre", poblacion.getNombre())
 					.append("poblacion", poblacion.getCodPoblacion())
-					.append("capital", capitalId); // Usar el ID numérico de la capital
-
+					.append("capital", capitalId); 
 			poblacionesCollection.insertOne(nuevaPoblacion);
 		}
 
@@ -477,38 +362,24 @@ public class Principal {
 		MongoCollection<Document> poblacionesCollection = database.getCollection("poblaciones");
 		MongoCollection<Document> capitalesCollection = database.getCollection("capitales");
 
-		// Buscar la población en MongoDB
 		Document poblacionMongoDB = poblacionesCollection
 				.find(Filters.eq("nombre", poblacion.getNombre())).first();
 
-		// Si la población no existe, crearla
 		if (poblacionMongoDB == null) {
-			// Buscar la capital en MongoDB
 			Document capitalMongoDB = capitalesCollection
 					.find(Filters.eq("nombre", poblacion.getCapitales().getNombre())).first();
 
 			Document nuevaPoblacion = new Document().append("nombre", poblacion.getNombre())
 					.append("poblacion", poblacion.getCodPoblacion())
-					.append("capital", capitalMongoDB); // Usar la capital como un subdocumento
+					.append("capital", capitalMongoDB); 
 
 			poblacionesCollection.insertOne(nuevaPoblacion);
 		}
 	}
 
 	// ------------------------------------------------------------
-	// ------------------------------------------------------------
-	// ------------------------------------------------------------
-	// ------------------------------------------------------------
-	// ------------------------------------------------------------
 	// ------------------- CRUD MONGODB ---------------------------
 	// ------------------------------------------------------------
-	// ------------------------------------------------------------
-	// ------------------------------------------------------------
-	// ------------------------------------------------------------
-	// ------------------------------------------------------------
-
-	// ------------------- Está hecho con pronvincias pero se puede cambiar a lo que
-	// haga falta ------------------
 	public Document leerProvincia(String nombre) {
 		MongoCollection<Document> provinciasCollection = database.getCollection("provincias");
 		Document provincia = provinciasCollection.find(Filters.eq("nombre", nombre)).first();
